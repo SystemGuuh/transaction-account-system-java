dfbd8304d560f74ffb5f3baef73d7d8938623e15
Gustavo Almeida <gustavo.almeida@eshows.com.br>
2024-04-23 01:27:17 -0300

    refactor: clear main

--- src/main/java/org/features/TransactionSystem.java
+++ src/main/java/org/features/TransactionSystem.java
@@ -3,16 +3,6 @@ import java.util.Random;
 
 public class TransactionSystem {
     public static void main(String[] args) {
-        Account account = new Account(true, 1000);
-        Random random = new Random();
-
-        for (int i = 0; i < 5; i++) {
-            double amount = (random.nextDouble() * 2000.0) - 1000.0;
-            amount = Math.round(amount * 100.0) / 100.0;
-            Transaction transaction = new Transaction(amount, "Pix", "2022-04-14T14:30:00");
-            account.addTransaction(transaction);
-        }
-
-        System.out.println(account);
+        //do something
     }
 }
