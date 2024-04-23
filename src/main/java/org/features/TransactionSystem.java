d014686b2f033433fe7265d903fd05877ac96637
Gustavo Almeida <gustavo.almeida@eshows.com.br>
2024-04-23 01:26:11 -0300

    refactor: remove thread and semaphore from main

--- src/main/java/org/features/TransactionSystem.java
+++ src/main/java/org/features/TransactionSystem.java
@@ -1,41 +1,16 @@
 package org.features;
 import java.util.Random;
-import java.util.concurrent.Semaphore;
 
 public class TransactionSystem {
     public static void main(String[] args) {
         Account account = new Account(true, 1000);
         Random random = new Random();
-        Semaphore semaphore = new Semaphore(1);
-        Thread accountThread = new Thread(account);
-        accountThread.start();
 
-        Thread[] threads = new Thread[5];
-
-        for (int i = 0; i < threads.length; i++) {
+        for (int i = 0; i < 5; i++) {
             double amount = (random.nextDouble() * 2000.0) - 1000.0;
             amount = Math.round(amount * 100.0) / 100.0;
-            Transaction transaction = new Transaction(amount, "Amazon", "2022-04-14T14:30:00");
-            threads[i] = new Thread(() -> {
-                try {
-                    semaphore.acquire();
-                    account.addTransaction(transaction);
-                    System.out.println("Transaction added by thread: " + Thread.currentThread().getName());
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                } finally {
-                    semaphore.release();
-                }
-            });
-            threads[i].start();
-        }
-
-        for (Thread thread : threads) {
-            try {
-                thread.join();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+            Transaction transaction = new Transaction(amount, "Pix", "2022-04-14T14:30:00");
+            account.addTransaction(transaction);
         }
 
         System.out.println(account);
