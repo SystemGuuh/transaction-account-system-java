8e9ee70fdf75b25f4ba3fae5a7e533d6889b8364
Gustavo Almeida <gustavo.almeida@eshows.com.br>
2024-04-23 13:29:23 -0300

    build: adding authorizations models as strategy pattern

--- src/main/java/org/features/authorization/FirstTransactionThresholdStrategy.java
+++ src/main/java/org/features/authorization/FirstTransactionThresholdStrategy.java
@@ -6,9 +6,7 @@ import org.features.Transaction;
 public class FirstTransactionThresholdStrategy implements AuthorizationStrategy {
     @Override
     public AuthorizationResult authorize(Transaction transaction, Account account) {
-        if (account.getHistory().isEmpty() && transaction.getAmount() > account.getAvailableLimit() * 0.9) {
-            return new AuthorizationResult(account, "first-transaction-above-threshold");
-        }
+
         return null;
     }
 }
